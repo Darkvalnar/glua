94e51ce36e11c42f65969f97616aa0cc8dd6201e
Darkvalnar <57048928+Darkvalnar@users.noreply.github.com>
2020-10-17 08:53:57 +0200

    Direct upload to main
    Code provided as is, no further changes have to be made.

---
+++ gasmask npc/lua/autorun/gasmask.lua
@@ -0,0 +1,7 @@
+--Sorry Phill but I had to directly rip this file from your original addon, mostly due to me being retarded.
+
+player_manager.AddValidModel( "gasmaskman", "models/player/phill/phoenix_marine_gasmask.mdl" );
+list.Set( "PlayerOptionsModel", "gasmaskman", "models/player/phill/phoenix_marine_gasmask.mdl" );
+player_manager.AddValidHands( "gasmaskman", "models/player/phill/c_arms_hev.mdl", 0, "00000000" )
+
+
