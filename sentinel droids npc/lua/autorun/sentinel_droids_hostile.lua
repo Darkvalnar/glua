a5c640a39a5c4909a202cbdcb25df9c7106de58e
Darkvalnar <57048928+Darkvalnar@users.noreply.github.com>
2020-10-17 09:07:19 +0200

    Add files via upload

---
+++ sentinel droids npc/lua/autorun/sentinel_droids_hostile.lua
@@ -0,0 +1,92 @@
+local NPC = {
+	Name = "Sentinel_droid1",
+	Class = "npc_combine_s",
+	Category = "Sentinel Droids (HOSTILE)",
+	Model = "models/npc/sentinel_droid1/sentinel_droid1.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid1", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid2",
+	Class = "npc_combine_s",
+	Category = "Sentinel Droids (HOSTILE)",
+	Model = "models/npc/sentinel_droid2/sentinel_droid2.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid2", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid3",
+	Class = "npc_combine_s",
+	Category = "Sentinel Droids (HOSTILE)",
+	Model = "models/npc/sentinel_droid3/sentinel_droid3.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid3", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid4",
+	Class = "npc_combine_s",
+	Category = "Sentinel Droids (HOSTILE)",
+	Model = "models/npc/sentinel_droid4/sentinel_droid4.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid4", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid5",
+	Class = "npc_combine_s",
+	Category = "Sentinel Droids (HOSTILE)",
+	Model = "models/npc/sentinel_droid5/sentinel_droid5.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid5", NPC )
+
+--------------------------------------------------FRIENDLY------------------------------------------------------
+local NPC = {
+	Name = "Sentinel_droid1",
+	Class = "npc_citizen",
+	Category = "Sentinel Droids (FRIENDLY)",
+	Model = "models/npc/sentinel_droid1/sentinel_droid1.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid1f", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid2",
+	Class = "npc_citizen",
+	Category = "Sentinel Droids (FRIENDLY)",
+	Model = "models/npc/sentinel_droid2/sentinel_droid2.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid2f", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid3",
+	Class = "npc_citizen",
+	Category = "Sentinel Droids (FRIENDLY)",
+	Model = "models/npc/sentinel_droid3/sentinel_droid3.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid3f", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid4",
+	Class = "npc_citizen",
+	Category = "Sentinel Droids (FRIENDLY)",
+	Model = "models/npc/sentinel_droid4/sentinel_droid4.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid4f", NPC )
+
+local NPC = {
+	Name = "Sentinel_droid5",
+	Class = "npc_citizen",
+	Category = "Sentinel Droids (FRIENDLY)",
+	Model = "models/npc/sentinel_droid5/sentinel_droid5.mdl",
+	Health = 100
+}
+list.Set( "NPC", "Sentinel_droid5f", NPC )
+
+
